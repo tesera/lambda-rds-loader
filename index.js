4685536e027cf22f11877442ef10e8b4cd91d955
Boris Cosic <boris.cosic@tesera.com>
2015-12-23 20:26:29 -0700

    Initial import.

---
+++ index.js
@@ -0,0 +1,197 @@
+var region = process.env['AWS_REGION'];
+if (!region || region === null || region === "") {
+    region = "us-east-1";
+    console.log("AWS Lambda RDS Database Loader using default region " + region);
+}
+
+var aws = require('aws-sdk');
+aws.config.update({
+    region : region
+});
+var s3 = new aws.S3({
+    apiVersion : '2006-03-01',
+    region : region
+});
+var dynamoDB = new aws.DynamoDB({
+    apiVersion : '2012-08-10',
+    region : region
+});
+
+require('./lib/constants');
+var common = require('./lib/common');
+var kmsCrypto = require('./lib/kmsCrypto');
+kmsCrypto.setRegion(region);
+var S3S = require('s3-streams');
+var pg = require('pg');
+var pgCopy = require('pg-copy-streams').from;
+var parse = require('csv-parse');
+var async = require('async');
+
+exports.handler = function(event, context) {
+    
+    exports.get_table_name = function(inputInfo) {
+        var table = inputInfo.key.split('/');
+        table = table[inputInfo.folderDepthLevelForTableName];
+        return table;
+    };
+
+    exports.create_table_if_not_exists = function(inputInfo, callback) {
+        if(inputInfo.tableName === null) {
+            var msg = 'Table name cannot be null.';
+            console.log(msg);
+            context.done(null, msg);
+        } else {
+            s3.getObject({Bucket: inputInfo.bucket, Key: inputInfo.key}, function(err, response) {
+                if(err) {
+                    console.log(err);
+                    context.done(err, null);
+                }
+                parse(response.Body.toString(), {delimiter: inputInfo.delimiter}, function(err, output) {
+                    var sql = "CREATE TABLE IF NOT EXISTS " + inputInfo.tableName + "(";
+                    var columns = [];
+                    for(var i = 0; i < output[0].length; i++) {
+                        columns.push(output[0][i] + " numeric");
+                    }
+
+                    if(columns.length === 0) {
+                        context.done(null, 'Column count for new table cannot be 0.');
+                    } else {
+                        pg.connect(inputInfo.connectionString, function(err, client) {
+                            if(err) {
+                                context.done(err, null);
+                            }
+                            sql += columns.join(',') + ')';
+                            console.log(sql)
+                            client.query(sql);
+                            callback();
+                        });
+                    }
+                });
+            });
+        }
+    };
+
+    exports.foundConfig = function(inputInfo, err, data) {
+        if (err) {
+            console.log(err);
+            var msg = 'Error getting RDS Configuration for ' + inputInfo.bucket + ' from Dynamo DB ';
+            console.log(msg);
+            context.done(error, msg);
+        } else {
+            if (!data || !data.Item) {
+                console.log("No Configuration Found for " + inputInfo.bucket);
+                context.done(null, null);
+            } else {
+                console.log("Found Redshift RDS Configuration for " + inputInfo.bucket);
+                var config = data.Item;
+
+                var encryptedItems = {};
+                encryptedItems['rdsPassword'] = kmsCrypto.stringToBuffer(config.loadRDS.L[0].M.connectPassword.S);
+                kmsCrypto.decryptMap(encryptedItems, function(err, decryptedConfigItems) {
+                    if (err) {
+                        context.done(error, 'Error decrypting configuration.');
+                    } else {
+                        inputInfo.connectionString = 'postgres://' + config.loadRDS.L[0].M.connectUser.S + ':' + decryptedConfigItems.rdsPassword.toString() + '@' 
+                            + config.loadRDS.L[0].M.rdsHost.S + ':' + config.loadRDS.L[0].M.rdsPort.N + '/' + config.loadRDS.L[0].M.rdsDB.S;
+                        inputInfo.schema = config.loadRDS.L[0].M.targetSchema.S;
+                        exports.runImport(inputInfo);
+                    }
+                });
+            }
+        }
+    };
+
+    exports.runImport = function(inputInfo) {
+        for(var i = 0; i < event.Records.length; i++) {
+            if(i === 0) {
+                inputInfo.key = event.Records[i].s3.object.key;
+                var name = exports.get_table_name(inputInfo);
+                inputInfo.tableName = (inputInfo.schema.length > 0 ? inputInfo.schema + '.' + name : name);
+                console.log('Table to import data into: ' + inputInfo.tableName);
+                
+                exports.create_table_if_not_exists(inputInfo, function() {
+                    var stream = S3S.ReadStream(s3, {Bucket: inputInfo.bucket, Key: inputInfo.key});
+                    pg.connect(inputInfo.connectionString, function(err, client) {
+                        if (err) console.log(err);
+                        var query = client.query(pgCopy(
+                            "COPY " + inputInfo.tableName + " FROM STDIN WITH CSV HEADER DELIMITER '" + inputInfo.delimiter + "'"
+                        ));
+                        stream.pipe(query)
+                            .on('end', function () {
+                                client.end();
+                                context.done();
+                            })
+                            .on('error', function(error) {
+                                console.log(error);
+                            });
+                    });
+                });
+            }
+        }
+    };
+
+    if(!event.Records) {
+        console.log('No records found');
+        context.done(null, null);
+    } else {
+        var inputInfo = {
+            bucket: event.Records[0].s3.bucket.name,
+            schema: 'public',
+            folderDepthLevelForTableName: 0,
+            tableName: null,
+            connectionString: null,
+            delimiter: ','
+        };
+
+        // load the configuration for this prefix
+        var dynamoLookup = {
+            Key : {
+                s3Prefix : {
+                    S : inputInfo.bucket
+                }
+            },
+            TableName : configTable,
+            ConsistentRead : true
+        };
+
+        var proceed = false;
+        var lookupConfigTries = 10;
+        var tryNumber = 0;
+        var configData = null;
+
+        async.whilst(function() {
+            // return OK if the proceed flag has been set, or if we've hit the retry count
+            return !proceed && tryNumber < lookupConfigTries;
+        }, function(callback) {
+            tryNumber++;
+
+            // lookup the configuration item, and run foundConfig on completion
+            dynamoDB.getItem(dynamoLookup, function(err, data) {
+                if (err) {
+                    if (err.code === provisionedThroughputExceeded) {
+                        // sleep for bounded jitter time up to 1 second and then retry
+                        var timeout = common.randomInt(0, 1000);
+                        console.log(provisionedThroughputExceeded + " while accessing " + configTable + ". Retrying in " + timeout + " ms");
+                        setTimeout(callback, timeout);
+                    } else {
+                        // some other error - call the error
+                        callback(err);
+                    }
+                } else {
+                    configData = data;
+                    proceed = true;
+                    callback(null);
+                }
+            });
+        }, function(err) {
+            if (err) {
+                // fail the context as we haven't been able to lookup the onfiguration
+                console.log(err);
+                context.done(error, err);
+            } else {
+                // call the foundConfig method with the data item
+                exports.foundConfig(inputInfo, null, configData);
+            }
+        });
+    }
+};
---
+++ setup.js
@@ -0,0 +1,245 @@
+/*
+		Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/asl/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and limitations under the License. 
+ */
+
+/**
+ * Ask questions of the end user via STDIN and then setup the DynamoDB table
+ * entry for the configuration when done
+ */
+var pjson = require('./package.json');
+var readline = require('readline');
+var aws = require('aws-sdk');
+require('./lib/constants');
+var common = require('./lib/common');
+var async = require('async');
+var uuid = require('node-uuid');
+var dynamoDB;
+var kmsCrypto = require('./lib/kmsCrypto');
+var setRegion;
+
+dynamoConfig = {
+	TableName : configTable,
+	Item : {
+		truncateTarget : {
+			BOOL : false
+		},
+		currentBatch : {
+			S : uuid.v4()
+		},
+		version : {
+			S : pjson.version
+		},
+		loadRDS : {
+			L : [ {
+				M : {
+
+				}
+			} ]
+		}
+	}
+};
+
+/* configuration of question prompts and config assignment */
+var rl = readline.createInterface({
+	input : process.stdin,
+	output : process.stdout
+});
+
+var qs = [];
+
+q_region = function(callback) {
+	rl.question('Enter the Region for the Configuration > ', function(answer) {
+		if (common.blank(answer) !== null) {
+			common.validateArrayContains([ "ap-northeast-1", "ap-southeast-1", "ap-southeast-2", "eu-central-1", "eu-west-1", "sa-east-1", "us-east-1", "us-west-1", "us-west-2" ],
+					answer.toLowerCase(), rl);
+
+			setRegion = answer.toLowerCase();
+
+			// configure dynamo db and kms for the correct region
+			dynamoDB = new aws.DynamoDB({
+				apiVersion : '2012-08-10',
+				region : setRegion
+			});
+			kmsCrypto.setRegion(setRegion);
+
+			callback(null);
+		}
+	});
+};
+
+q_s3Prefix = function(callback) {
+	rl.question('Enter the S3 Bucket > ', function(answer) {
+		common.validateNotNull(answer, 'You Must Provide an S3 Bucket Name', rl);
+
+		// setup prefix to be * if one was not provided
+		var stripped = answer.replace(new RegExp('s3://', 'g'), '');
+		var elements = stripped.split("/");
+		var setPrefix = undefined;
+
+		if (elements.length === 1) {
+			// bucket only so use "bucket" alone
+			setPrefix = elements[0];
+		} else {
+			// right trim "/"
+			setPrefix = stripped.replace(/\/$/, '');
+		}
+
+		dynamoConfig.Item.s3Prefix = {
+			S : setPrefix
+		};
+
+		callback(null);
+	});
+};
+
+q_filenameFilter = function(callback) {
+	rl.question('Enter a Filename Filter Regex > ', function(answer) {
+		if (common.blank(answer) !== null) {
+			dynamoConfig.Item.filenameFilterRegex = {
+				S : answer
+			};
+		}
+		callback(null);
+	});
+};
+
+q_rdsHost = function(callback) {
+	rl.question('Enter the RDS Host > ', function(answer) {
+		common.validateNotNull(answer, 'You Must Provide an RDS Host', rl);
+		dynamoConfig.Item.loadRDS.L[0].M.rdsHost = {
+			S : answer
+		};
+		callback(null);
+	});
+};
+
+q_rdsPort = function(callback) {
+	rl.question('Enter the RDS Port > ', function(answer) {
+		dynamoConfig.Item.loadRDS.L[0].M.rdsPort = {
+			N : '' + common.getIntValue(answer, rl)
+		};
+		callback(null);
+	});
+};
+
+q_rdsDB = function(callback) {
+	rl.question('Enter the Database Name > ', function(answer) {
+		if (common.blank(answer) !== null) {
+			dynamoConfig.Item.loadRDS.L[0].M.rdsDB = {
+				S : answer
+			};
+		}
+		callback(null);
+	});
+};
+
+q_userName = function(callback) {
+	rl.question('Enter the Database Username > ', function(answer) {
+		common.validateNotNull(answer, 'You Must Provide a Username', rl);
+		dynamoConfig.Item.loadRDS.L[0].M.connectUser = {
+			S : answer
+		};
+		callback(null);
+	});
+};
+
+q_userPwd = function(callback) {
+	rl.question('Enter the Database Password > ', function(answer) {
+		common.validateNotNull(answer, 'You Must Provide a Password', rl);
+
+		kmsCrypto.encrypt(answer, function(err, ciphertext) {
+			if (err) {
+				console.log(JSON.stringify(err));
+				process.exit(ERROR);
+			} else {
+				dynamoConfig.Item.loadRDS.L[0].M.connectPassword = {
+					S : kmsCrypto.toLambdaStringFormat(ciphertext)
+				};
+				callback(null);
+			}
+		});
+	});
+};
+
+q_schema = function(callback) {
+	rl.question('Enter the Schema (optional) > ', function(answer) {
+		if (answer && answer !== null && answer !== "") {
+			dynamoConfig.Item.loadRDS.L[0].M.targetSchema = {
+				S : answer
+			};
+			callback(null);
+		} else {
+			callback(null);
+		}
+	});
+};
+
+q_truncateTable = function(callback) {
+	rl.question('Should the Table be Truncated before Load? (Y/N) > ', function(answer) {
+		dynamoConfig.Item.loadRDS.L[0].M.truncateTarget = {
+			BOOL : common.getBooleanValue(answer)
+		};
+		callback(null);
+	});
+};
+
+q_csvDelimiter = function(callback) {
+	//if (dynamoConfig.Item.dataFormat.S === 'CSV') {
+		rl.question('Enter the CSV Delimiter > ', function(answer) {
+			common.validateNotNull(answer, 'You Must the Delimiter for CSV Input', rl);
+			dynamoConfig.Item.csvDelimiter = {
+				S : answer
+			};
+			callback(null);
+		});
+	//} else {
+	//	callback(null);
+	//}
+};
+
+last = function(callback) {
+	rl.close();
+
+	setup(null, callback);
+};
+
+setup = function(overrideConfig, callback) {
+	// set which configuration to use
+	var useConfig = undefined;
+	if (overrideConfig) {
+		useConfig = overrideConfig;
+	} else {
+		useConfig = dynamoConfig;
+	}
+	var configWriter = common.writeConfig(setRegion, dynamoDB, useConfig, callback);
+	common.createTables(dynamoDB, configWriter);
+};
+// export the setup module so that customers can programmatically add new
+// configurations
+exports.setup = setup;
+
+qs.push(q_region);
+qs.push(q_s3Prefix);
+qs.push(q_filenameFilter);
+qs.push(q_rdsHost);
+qs.push(q_rdsPort);
+qs.push(q_rdsDB);
+qs.push(q_schema);
+qs.push(q_truncateTable);
+qs.push(q_userName);
+qs.push(q_userPwd);
+qs.push(q_csvDelimiter);
+
+// always have to have the 'last' function added to halt the readline channel
+// and run the setup
+qs.push(last);
+
+// call the first function in the function list, to invoke the callback
+// reference chain
+async.waterfall(qs);
